f42a95d21f413a6398a3cd3681bf92e4b066ba94
czYkn <244115113@qq.com>
2018-11-11 19:15:16 +0800

    add citylist

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -4,30 +4,7 @@
         <city-search></city-search>
         <city-list :hotCity="hotCity"
                    :citySort="citySort"
-                   :cityAs="cityAs"
-                   :cityBs="cityBs"
-                   :cityCs="cityCs"
-                   :cityDs="cityDs"
-                   :cityEs="cityEs"
-                   :cityFs="cityFs"
-                   :cityGs="cityGs"
-                   :cityHs="cityHs"
-                   :cityJs="cityJs"
-                   :cityKs="cityKs"
-                   :cityLs="cityLs"
-                   :cityMs="cityMs"
-                   :cityNs="cityNs"
-                   :cityPs="cityPs"
-                   :cityQs="cityQs"
-                   :cityRs="cityRs"
-                   :citySs="citySs"
-                   :cityTs="cityTs"
-                   :cityUs="cityUs"
-                   :cityVs="cityVs"
-                   :cityWs="cityWs"
-                   :cityXs="cityXs"
-                   :cityYs="cityYs"
-                   :cityZs="cityZs"
+                   :cityAll="cityAll"
         ></city-list>
     </div>
 </template>
@@ -48,29 +25,7 @@ export default {
     return {
       hotCity: [],
       citySort: [],
-      cityAs: [],
-      cityBs: [],
-      cityDs: [],
-      cityEs: [],
-      cityFs: [],
-      cityGs: [],
-      cityHs: [],
-      cityJs: [],
-      cityKs: [],
-      cityLs: [],
-      cityMs: [],
-      cityNs: [],
-      cityPs: [],
-      cityQs: [],
-      cityRs: [],
-      citySs: [],
-      cityTs: [],
-      cityUs: [],
-      cityVs: [],
-      cityWs: [],
-      cityXs: [],
-      cityYs: [],
-      cityZs: []
+      cityAll: {}
     }
   },
   methods: {
@@ -83,31 +38,7 @@ export default {
       if (res.ret && res.data) {
         this.hotCity = res.data.hotCities
         this.citySort = res.data.citySort
-        this.cityAs = res.data.cities.A
-        this.cityBs = res.data.cities.B
-        this.cityCs = res.data.cities.C
-        this.cityDs = res.data.cities.D
-        this.cityEs = res.data.cities.E
-        this.cityFs = res.data.cities.F
-        this.cityGs = res.data.cities.G
-        this.cityHs = res.data.cities.H
-        this.cityJs = res.data.cities.J
-        this.cityKs = res.data.cities.K
-        this.cityLs = res.data.cities.L
-        this.cityMs = res.data.cities.M
-        this.cityNs = res.data.cities.N
-        this.cityOs = res.data.cities.O
-        this.cityPs = res.data.cities.P
-        this.cityQs = res.data.cities.Q
-        this.cityRs = res.data.cities.R
-        this.citySs = res.data.cities.S
-        this.cityTs = res.data.cities.T
-        this.cityUs = res.data.cities.U
-        this.cityVs = res.data.cities.V
-        this.cityWs = res.data.cities.W
-        this.cityXs = res.data.cities.X
-        this.cityYs = res.data.cities.Y
-        this.cityZs = res.data.cities.Z
+        this.cityAll = res.data.cities
       }
     }
   },
--- src/pages/city/components/List.vue
+++ src/pages/city/components/List.vue
@@ -17,182 +17,17 @@
                     :key="index"
                 ></li>
             </ul>
-            <div class="a-city">A</div>
-            <ul class="a-city-ul">
-                <li class="a-city-li"
-                    v-for="city of cityAs"
-                    v-text="city.name"
-                    :key="city.id"
-                ></li>
-            </ul>
-            <div class="a-city">B</div>
-            <ul class="a-city-ul">
-                <li class="a-city-li"
-                    v-for="city of cityBs"
-                    v-text="city.name"
-                    :key="city.id"
-                ></li>
-            </ul>
-            <div class="a-city">C</div>
-            <ul class="a-city-ul">
-                <li class="a-city-li"
-                    v-for="city of cityCs"
-                    v-text="city.name"
-                    :key="city.id"
-                ></li>
-            </ul>
-            <div class="a-city">D</div>
-            <ul class="a-city-ul">
-                <li class="a-city-li"
-                    v-for="city of cityDs"
-                    v-text="city.name"
-                    :key="city.id"
-                ></li>
-            </ul>
-            <div class="a-city">E</div>
-            <ul class="a-city-ul">
-                <li class="a-city-li"
-                    v-for="city of cityEs"
-                    v-text="city.name"
-                    :key="city.id"
-                ></li>
-            </ul>
-            <div class="a-city">F</div>
-            <ul class="a-city-ul">
-                <li class="a-city-li"
-                    v-for="city of cityFs"
-                    v-text="city.name"
-                    :key="city.id"
-                ></li>
-            </ul>
-            <div class="a-city">G</div>
-            <ul class="a-city-ul">
-                <li class="a-city-li"
-                    v-for="city of cityGs"
-                    v-text="city.name"
-                    :key="city.id"
-                ></li>
-            </ul>
-            <div class="a-city">H</div>
-            <ul class="a-city-ul">
-                <li class="a-city-li"
-                    v-for="city of cityHs"
-                    v-text="city.name"
-                    :key="city.id"
-                ></li>
-            </ul>
-            <div class="a-city">J</div>
-            <ul class="a-city-ul">
-                <li class="a-city-li"
-                    v-for="city of cityJs"
-                    v-text="city.name"
-                    :key="city.id"
-                ></li>
-            </ul>
-            <div class="a-city">K</div>
-            <ul class="a-city-ul">
-                <li class="a-city-li"
-                    v-for="city of cityKs"
-                    v-text="city.name"
-                    :key="city.id"
-                ></li>
-            </ul>
-            <div class="a-city">L</div>
-            <ul class="a-city-ul">
-                <li class="a-city-li"
-                    v-for="city of cityLs"
-                    v-text="city.name"
-                    :key="city.id"
-                ></li>
-            </ul>
-            <div class="a-city">M</div>
-            <ul class="a-city-ul">
-                <li class="a-city-li"
-                    v-for="city of cityMs"
-                    v-text="city.name"
-                    :key="city.id"
-                ></li>
-            </ul>
-            <div class="a-city">N</div>
-            <ul class="a-city-ul">
-                <li class="a-city-li"
-                    v-for="city of cityNs"
-                    v-text="city.name"
-                    :key="city.id"
-                ></li>
-            </ul>
-            <div class="a-city">P</div>
-            <ul class="a-city-ul">
-                <li class="a-city-li"
-                    v-for="city of cityPs"
-                    v-text="city.name"
-                    :key="city.id"
-                ></li>
-            </ul>
-            <div class="a-city">Q</div>
-            <ul class="a-city-ul">
-                <li class="a-city-li"
-                    v-for="city of cityQs"
-                    v-text="city.name"
-                    :key="city.id"
-                ></li>
-            </ul>
-            <div class="a-city">R</div>
-            <ul class="a-city-ul">
-                <li class="a-city-li"
-                    v-for="city of cityRs"
-                    v-text="city.name"
-                    :key="city.id"
-                ></li>
-            </ul>
-            <div class="a-city">S</div>
-            <ul class="a-city-ul">
-                <li class="a-city-li"
-                    v-for="city of citySs"
-                    v-text="city.name"
-                    :key="city.id"
-                ></li>
-            </ul>
-            <div class="a-city">T</div>
-            <ul class="a-city-ul">
-                <li class="a-city-li"
-                    v-for="city of cityBs"
-                    v-text="city.name"
-                    :key="city.id"
-                ></li>
-            </ul>
-            <div class="a-city">W</div>
-            <ul class="a-city-ul">
-                <li class="a-city-li"
-                    v-for="city of cityWs"
-                    v-text="city.name"
-                    :key="city.id"
-                ></li>
-            </ul>
-            <div class="a-city">X</div>
-            <ul class="a-city-ul">
-                <li class="a-city-li"
-                    v-for="city of cityXs"
-                    v-text="city.name"
-                    :key="city.id"
-                ></li>
-            </ul>
-            <div class="a-city">Y</div>
-            <ul class="a-city-ul">
-                <li class="a-city-li"
-                    v-for="city of cityYs"
-                    v-text="city.name"
-                    :key="city.id"
-                ></li>
-            </ul>
-            <div class="a-city">Z</div>
-            <ul class="a-city-ul">
-                <li class="a-city-li"
-                    v-for="city of cityZs"
-                    v-text="city.name"
-                    :key="city.id"
-                ></li>
-            </ul>
+            <div v-for="(city,key) of cityAll" :key="key">
+                <div class="a-city" v-text="key"></div>
+                <ul class="a-city-ul">
+                    <li class="a-city-li"
+                        v-for="item of city"
+                        v-text="item.name"
+                        :key="item.id"
+                    >
+                    </li>
+                </ul>
+            </div>
         </div>
     </div>
 </template>
@@ -204,29 +39,7 @@ export default {
   props: {
     hotCity: Array,
     citySort: Array,
-    cityAs: Array,
-    cityBs: Array,
-    cityCs: Array,
-    cityDs: Array,
-    cityEs: Array,
-    cityFs: Array,
-    cityGs: Array,
-    cityHs: Array,
-    cityIs: Array,
-    cityJs: Array,
-    cityKs: Array,
-    cityLs: Array,
-    cityMs: Array,
-    cityNs: Array,
-    cityPs: Array,
-    cityQs: Array,
-    cityRs: Array,
-    citySs: Array,
-    cityTs: Array,
-    cityWs: Array,
-    cityXs: Array,
-    cityYs: Array,
-    cityZs: Array
+    cityAll: Object
   },
   mounted () {
     // vue 完全加载完毕才会执行的钩子函数
