279b2f2f61a3c8a3dd22dcd9518cc004d252f650
czYkn <244115113@qq.com>
2018-11-13 12:24:53 +0800

    add keep-alive

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <template>
   <div id="app">
-    <!-- 显示当前路由所对应的组件 -->
-    <router-view/>
+    <!-- 我路由的内容被加载过一次之后，将其放在内存中，下一次再进入此路由，不需要再重新加载 将内存的取出来用就可以 -->
+    <keep-alive>
+      <!-- 显示当前路由所对应的组件 -->
+      <router-view/>
+    </keep-alive>
   </div>
 </template>
 
--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -15,6 +15,7 @@ import HomeIcons from './components/Icons'
 import HomeRecommend from './components/Recommend'
 import HomeWeekend from './components/Weekend'
 import axios from 'axios'
+import { mapState } from 'vuex'
 export default {
   name: 'Home',
   components: {
@@ -26,6 +27,7 @@ export default {
   },
   data () {
     return {
+      lastCity: '',
       swiperList: [],
       iconsList: [],
       recommendList: [],
@@ -34,7 +36,7 @@ export default {
   },
   methods: {
     getHomeInfo () {
-      axios.get('/api/index.json')
+      axios.get('/api/index.json?city=' + this.city)
         .then(this.getHomeInfoSucc)
     },
     getHomeInfoSucc (res) {
@@ -47,8 +49,19 @@ export default {
       }
     }
   },
+  computed: {
+    ...mapState(['city'])
+  },
   mounted () {
+    // 页面初次加载时执行的钩子函数
+    this.lastCity = this.city
     this.getHomeInfo()
+  },
+  activated () {
+    if (this.lastCity !== this.city) {
+      this.lastCity = this.city
+      this.getHomeInfo()
+    }
   }
 }
 </script>
